bcd25d7585e9d988349adb2739378e8283d85aa9
Thomas Wearmouth <tomwmth@pm.me>
2024-10-28 01:51:11 +0800

    Remove unused property description defaults

--- src/main/java/com/github/lunatrius/schematica/reference/Names.java
+++ src/main/java/com/github/lunatrius/schematica/reference/Names.java
@@ -14,64 +14,37 @@ public final class Names {
         }
 
         public static final String DUMP_BLOCK_LIST = "dumpBlockList";
-        public static final String DUMP_BLOCK_LIST_DESC = "Dump all block states on startup.";
         public static final String SHOW_DEBUG_INFO = "showDebugInfo";
-        public static final String SHOW_DEBUG_INFO_DESC = "Display extra information on the debug screen (F3).";
 
         public static final String ALPHA_ENABLED = "alphaEnabled";
-        public static final String ALPHA_ENABLED_DESC = "Enable transparent textures.";
         public static final String ALPHA = "alpha";
-        public static final String ALPHA_DESC = "Alpha value used when rendering the schematic (1.0 = opaque, 0.5 = half transparent, 0.0 = transparent).";
         public static final String HIGHLIGHT = "highlight";
-        public static final String HIGHLIGHT_DESC = "Highlight invalid placed blocks and to be placed blocks.";
         public static final String HIGHLIGHT_AIR = "highlightAir";
-        public static final String HIGHLIGHT_AIR_DESC = "Highlight blocks that should be air.";
         public static final String BLOCK_DELTA = "blockDelta";
-        public static final String BLOCK_DELTA_DESC = "Delta value used for highlighting (if you experience z-fighting increase this).";
         public static final String RENDER_DISTANCE = "renderDistance";
-        public static final String RENDER_DISTANCE_DESC = "Schematic render distance.";
 
         public static final String PLACE_DELAY = "placeDelay";
-        public static final String PLACE_DELAY_DESC = "Delay between placement attempts (in ticks).";
         public static final String TIMEOUT = "timeout";
-        public static final String TIMEOUT_DESC = "Timeout before re-trying failed blocks.";
         public static final String PLACE_DISTANCE = "placeDistance";
-        public static final String PLACE_DISTANCE_DESC = "Maximum placement distance.";
         public static final String PLACE_INSTANTLY = "placeInstantly";
-        public static final String PLACE_INSTANTLY_DESC = "Place all blocks that can be placed in one tick.";
         public static final String DESTROY_BLOCKS = "destroyBlocks";
-        public static final String DESTROY_BLOCKS_DESC = "The printer will destroy blocks (creative mode only).";
         public static final String DESTROY_INSTANTLY = "destroyInstantly";
-        public static final String DESTROY_INSTANTLY_DESC = "Destroy all blocks that can be destroyed in one tick.";
         public static final String PLACE_ADJACENT = "placeAdjacent";
-        public static final String PLACE_ADJACENT_DESC = "Place blocks only if there is an adjacent block next to them.";
         public static final String SWAP_SLOTS = "swapSlot";
-        public static final String SWAP_SLOT_DESC = "Allow the printer to use this hotbar slot.";
 
         public static final String SCHEMATIC_DIRECTORY = "schematicDirectory";
-        public static final String SCHEMATIC_DIRECTORY_DESC = "Schematic directory.";
         public static final String EXTRA_AIR_BLOCKS = "extraAirBlocks";
-        public static final String EXTRA_AIR_BLOCKS_DESC = "Extra blocks to consider as air for the schematic renderer.";
         public static final String SORT_TYPE = "sortType";
-        public static final String SORT_TYPE_DESC = "Default sort type for the material list.";
 
         public static final String PRINTER_ENABLED = "printerEnabled";
-        public static final String PRINTER_ENABLED_DESC = "Allow players to use the printer.";
         public static final String SAVE_ENABLED = "saveEnabled";
-        public static final String SAVE_ENABLED_DESC = "Allow players to save schematics.";
         public static final String LOAD_ENABLED = "loadEnabled";
-        public static final String LOAD_ENABLED_DESC = "Allow players to load schematics.";
         public static final String PLAYER_QUOTA_KILOBYTES = "playerQuota";
-        public static final String PLAYER_QUOTA_KILOBYTES_DESC = "Amount of storage provided per-player for schematics on the server.";
 
         public static final String CHANGE_STATE = "changeState";
-        public static final String CHANGE_STATE_DESC = "Printer will interact with blocks to fix their state when necessary.";
         public static final String CHANGE_STATE_TIMEOUT = "changeStateTimeout";
-        public static final String CHANGE_STATE_TIMEOUT_DESC = "Timeout before performing another state change on a single block.";
         public static final String ARROW_KEY_MOVE = "arrowKeyMove";
-        public static final String ARROW_KEY_MOVE_DESC = "Pressing the arrow keys will move the currently loaded schematic.";
         public static final String PERSIST_SCHEMATIC = "persistSchematic";
-        public static final String PERSIST_SCHEMATIC_DESC = "Disconnecting from a server will not reset the loaded schematic.";
 
         public static final String LANG_PREFIX = Reference.MOD_ID + ".config";
     }
